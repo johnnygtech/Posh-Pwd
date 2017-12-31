6b9613bd504e88dbf6a853532187dfdf805058f5
John Gleason <johnnygtech@users.noreply.github.com>
2017-12-31 18:37:07 -0500

    moved tests.ps1 tests.ps1

--- functions.ps1
+++ functions.ps1
@@ -26,22 +26,3 @@ function New-Rfc2898StringHash
     $result.add("hash",[convert]::Tobase64string($hashFunc.GetBytes(20)))
     return $result
 }
-
-$stringToSalt = "password"
-
-#When you have to hash a NEW string
-$Obj = New-Rfc2898StringHash -string $stringToSalt -iterations 60000
-#When comparing a hash previously generated
-$testone = New-Rfc2898StringHash -string $stringToSalt -iterations $obj.iterations -salt $obj.salt
-#a new stored version of same original string
-$obj2 = New-Rfc2898StringHash -string $stringToSalt -iterations 60000
-#successfully matches new object
-$testtwo = New-Rfc2898StringHash -string $stringToSalt -iterations $obj2.iterations -salt $obj2.salt
-#however, original object and obj2 are different (due to the salt)
-$obj
-$testone
-$obj2
-$testtwo
-Compare-Object $testone.hash $testtwo.hash
-
-
